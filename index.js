8db899f608ed335f459005c529ef0e75310be952
Chiranjeev Veer Srivastava <chiranjeev.important@gmail.com>
2022-08-13 14:46:44 +0530

    Replaced to original after finding solution

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const routes = require("./src/routes");
 // Import Twilio and initialize the client.
 // IMPORTANT: Remember to set environment variables for your Account SID and Auth Token.
 const twilio = require("twilio");
-const accountSid = "ACa7e94b4cfdfe77171e498c9b11f48404";
-const authToken = "6319df634ea7b069b544c1e1f06a4c6a";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 // Configure the express application
